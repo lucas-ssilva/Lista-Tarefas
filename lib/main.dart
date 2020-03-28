4d1c15f5fe882cc418e44f3bfb267daa3b5159bf
Lucas <lucas_ssilva@outlook.com>
2020-03-27 21:15:16 -0300

    lista de tarefas

---
+++ lib/main.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'dart:io';
+import 'dart:convert';
+
+
+void main ()  {
+  
+  runApp(MaterialApp(
+    home: Home(),
+    ),
+  );
+}
+
+class Home extends StatefulWidget {
+  @override 
+  _HomeState createState() => _HomeState();
+}
+class _HomeState extends State<Home>{
+
+final toDoController = TextEditingController();
+
+  List _toDoList = [];
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+
+  @override 
+  void initState(){
+    super.initState();
+    _readData().then((data) {
+      setState(() {
+              _toDoList = jsonDecode(data);
+      });
+    });
+  }
+
+  void _addToDo(){
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+    newToDo["title"] = toDoController.text;
+    toDoController.clear();
+    newToDo["ok"] = false;
+    _toDoList.add(newToDo);
+    _saveData();
+    });
+  }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));//espera um segundo pra atualizar, perfumaria 
+    setState(() {
+      _toDoList.sort((a,b){
+      if(a["ok"] && !b["ok"]) return 1;
+      else if(!a["ok"] && b["ok"]) return -1;
+      else return 0;
+    });
+    _saveData();
+    });
+        return null;
+    }
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      title: Text("Lista de Tarefas", style: TextStyle(color: Colors.white),),
+      backgroundColor: Colors.blueAccent,centerTitle: true,
+    ),
+    body:  Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.fromLTRB(17, 1, 7, 1),
+          child: Row(children: <Widget>[
+            Expanded(child: TextField(controller:toDoController , decoration: InputDecoration(labelText: "Nova Tarefa", 
+            labelStyle: TextStyle(color: Colors.blueAccent)
+            ),
+            ),
+            ),
+            RaisedButton(
+              color: Colors.blueAccent,
+              child: Text("Add"),textColor: Colors.white,
+              onPressed: _addToDo,
+            )
+          ],),
+        ),
+        Expanded(
+          child: RefreshIndicator( onRefresh: _refresh,
+          child: ListView.builder(
+            padding: EdgeInsets.only(top:10),
+            itemCount: _toDoList.length ,
+             itemBuilder: buildItem
+          ),
+          )
+    )],),
+    )
+    ;
+  }
+
+
+Widget buildItem(context,index){
+               return Dismissible( //metodo que permite excluir
+                 key: Key(DateTime.now().millisecondsSinceEpoch.toString()),//cada exlusão precisa de uma chave, desse jeito pego o milisefgundo do tempo atual e transformo na chave que sera usada 
+                 background: Container(
+                   color: Colors.red,
+                   child: Align(
+                     alignment: Alignment(-0.9,0.0), // onde o item de excluir vai ficar
+                     child: Icon(Icons.delete_forever, color: Colors.white),
+                   ),
+                 ),
+                 direction: DismissDirection.startToEnd, // direção que o excluir vai 
+                 child: CheckboxListTile( //checjbox 
+                 title: Text(_toDoList[index]["title"]), 
+                 value: _toDoList[index] ["ok"],
+                 secondary: CircleAvatar(
+                   child: Icon( _toDoList[index] ["ok"] ? Icons.check : Icons.error) // validação que verifica se está marcado ou não o checkbox e muda o icone
+                    ,),
+                    onChanged: (c){
+                      setState(() {
+                        _toDoList[index] ["ok"] = c;
+                        _saveData();
+                      });
+                    },
+               ),
+               onDismissed: (direction) {
+                 setState(() {
+                   _lastRemoved = Map.from(_toDoList[index]);
+                 _lastRemovedPos = index;
+                 _toDoList.removeAt(index);
+                 _saveData();
+
+                 final snack = SnackBar(content: Text("Tarefa \"${_lastRemoved["title"]}\" Removida"),
+                 action: SnackBarAction(label: "Desfazer", 
+                 onPressed: () {
+                   setState(() {
+                     _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                   _saveData();
+                   });
+                 }),
+                 duration: Duration(seconds:10),
+                 );
+
+                 Scaffold.of(context).showSnackBar(snack);
+
+                 });
+               });
+               
+             }
+  Future<File> _getFile() async { //get application precisa do async pois não pega a permissão de acessar o diretorio na hora
+  final directory = await getApplicationDocumentsDirectory();
+  return File("${directory.path}/data.json");
+}
+
+Future<File> _saveData() async {
+String data = json.encode(_toDoList);
+final file = await _getFile();
+return file.writeAsString(data);
+}
+
+Future<String> _readData() async {
+  try {
+    final file = await _getFile();
+    return file.readAsStringSync();
+  }catch (e) {
+    return null;
+  }
+}
+
+}
+
